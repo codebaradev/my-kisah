c60e69795907825ae326c9d2546d980c04497415
codebaradev <bahnasep@gmail.com>
2026-02-14 13:37:14 +0800

    feat: Add Title and Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ['latin'] })
 const _geistMono = Geist_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Our Love Story',
+  title: 'My Kisah',
   description: 'A celebration of our beautiful journey together',
   generator: 'v0.app',
 }
